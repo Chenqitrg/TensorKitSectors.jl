3605a729a995ab6fa2e94433b5b1e52364444330
Chenqi Meng <mcqgravity@gmail.com>
2025-05-07 00:29:37 +0800

    Still trying to clarify the structure of the category

--- src/pointedfusion/pointedfusion.jl
+++ src/pointedfusion/pointedfusion.jl
@@ -1,15 +1,29 @@
 using TensorKit
-
+using Revise
 include("../groups.jl")
 include("groupelements.jl")
 
+abstract type FusionCategory end
+abstract type VecGω{G,ω} <: FusionCategory end
 
-struct H³{G<:Group}
-    ω::Function
+abstract type CohomologyGroup{N,G} <: AbelianGroup end
+getGroup(::Type{VecGω{G,ω}}) where {G<:Group,ω} = G
+struct Irr{𝒞<:FusionCategory} <: Sector
+    value::Any
+    function Irr{𝒞}(value) where {𝒞<:FusionCategory}
+        if 𝒞 <: VecGω
+            G = getGroup(𝒞)
+            if value isa GroupElement{G}
+                new(value)
+            else
+                throw(ArgumentError("Irr value must be a GroupElement of type $G"))
+            end
+        else
+            throw(ArgumentError("$𝒞 has not been implemented"))
+        end
+    end
 end
-
-# struct VecGω{G, ω} where {G<:Group, ω<:ThreeCocycle{G}}
-#     g::G
-# end
+ω(a, b, c) = 1
+Irr{VecGω{ℤ{3}, ω}}(GroupElement{ℤ{3}}(1))
 
 
